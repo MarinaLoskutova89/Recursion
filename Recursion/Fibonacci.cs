ebbff42dd2c351fef544a085488bc3ca18a00d9b
Loskutova Marina <mloskutova1589@gmail.com>
2024-08-08 12:13:28 +0300

    first commit

---
+++ Recursion/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public class Fibonacci
+    {
+        public int RecursionFibonacci(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return RecursionFibonacci(n - 1) + RecursionFibonacci(n - 2);
+        }
+        public int CycleFibonacсi(int n)
+        {
+            int result = 0;
+            int x = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = result;
+                result = x;
+                x += tmp;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputs = new List<int>() { 5, 10, 20 };
+            var st = new Stopwatch();
+            var fib = new Fibonacci();
+
+            foreach (int i in inputs) 
+            { 
+                st.Start();
+                int recFib = fib.RecursionFibonacci(i);
+                st.Stop();
+                Console.WriteLine($"Output: {recFib}");
+                Console.WriteLine($"Recursion time (value = {i}): {st.Elapsed}");
+                Console.WriteLine();
+
+                st.Restart();
+
+                st.Start();
+                int cyclFib = fib.CycleFibonacсi(i);
+                st.Stop();
+                Console.WriteLine($"Output: {cyclFib}");
+                Console.WriteLine($"Cycle time (value = {i}): {st.Elapsed}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
